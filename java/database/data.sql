b8447041725d46eb573060bc6502d49cbd87276b
Jacob Stephens <jstephens1215@gmail.com>
2024-06-03 10:55:44 -0400

    added dummy pet data to database scripts

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,27 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO pets (name, species, breed, size, gender, age, sprayed_neutered) VALUES ('Queso', 'Cat', 'Maine Coon', 'Large', 'Female', 'Adult', true);
+INSERT INTO pets (name, species, breed, size, gender, age, sprayed_neutered) VALUES ('Big Chew', 'Dog', 'Great Dane', 'Large', 'Male', 'Young', true);
+
+INSERT INTO description(description) VALUES ('Playful');
+INSERT INTO description(description) VALUES ('Affectionate');
+INSERT INTO description(description) VALUES ('Funny');
+INSERT INTO description(description) VALUES ('Intelligent');
+INSERT INTO description(description) VALUES ('Cute');
+INSERT INTO description(description) VALUES ('Independent');
+INSERT INTO description(description) VALUES ('Laidback');
+INSERT INTO description(description) VALUES ('Energetic');
+
+INSERT INTO pet_description(pet_id, description_id) VALUES (1001, 2002);
+INSERT INTO pet_description(pet_id, description_id) VALUES (1001, 2004);
+INSERT INTO pet_description(pet_id, description_id) VALUES (1001, 2005);
+INSERT INTO pet_description(pet_id, description_id) VALUES (1001, 2006);
+INSERT INTO pet_description(pet_id, description_id) VALUES (1001, 2007);
+INSERT INTO pet_description(pet_id, description_id) VALUES (1002, 2001);
+INSERT INTO pet_description(pet_id, description_id) VALUES (1002, 2002);
+INSERT INTO pet_description(pet_id, description_id) VALUES (1002, 2003);
+INSERT INTO pet_description(pet_id, description_id) VALUES (1002, 2005);
+INSERT INTO pet_description(pet_id, description_id) VALUES (1002, 2008);
+
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,8 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS pet_description;
+DROP TABLE IF EXISTS users, pets, description;
+DROP SEQUENCE IF EXISTS seq_pet_id, seq_description_id;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +10,43 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+---- PUT TABLE CREATION STATEMENTS HERE
+CREATE SEQUENCE seq_pet_id
+  INCREMENT BY 1
+  START WITH 1001
+  MAXVALUE 2000;
+
+create table pets (
+    pet_id int NOT NULL DEFAULT nextval('seq_pet_id'),
+    name VARCHAR(50) NOT NULL,
+    species VARCHAR(50) NOT NULL,
+    breed VARCHAR(50) NOT NULL,
+	size VARCHAR(50),
+	gender VARCHAR(50),
+	age VARCHAR(50) NOT NULL,
+	sprayed_neutered BOOLEAN,
+	CONSTRAINT pk_pets PRIMARY KEY (pet_id)
+);
+
+CREATE SEQUENCE seq_description_id
+  INCREMENT BY 1
+  START WITH 2001
+  MAXVALUE 3000;
+
+create table description (
+    description_id int NOT NULL DEFAULT nextval('seq_description_id'),
+    description VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_description PRIMARY KEY (description_id)
+);
+
+create table pet_description (
+    pet_id INT,
+    description_id INT,
+    CONSTRAINT pk_pet_description PRIMARY KEY (pet_id, description_id),
+    CONSTRAINT fk_pet_id FOREIGN KEY (pet_id) REFERENCES pets(pet_id),
+    CONSTRAINT fk_description_id FOREIGN KEY (description_id) REFERENCES description(description_id)
 );
 
 COMMIT TRANSACTION;
