fb760926c4bb1527e51edaa46a580d938a475d96
Jeremy McKeever <jeremymck@gmail.com>
2024-06-04 13:59:04 -0400

    added some test queries

--- java/database/test_queries.sql
+++ java/database/test_queries.sql
@@ -3,3 +3,25 @@
 ------------------------------------------------------
 
 SELECT * FROM users;
+
+
+SELECT * FROM descriptions;
+SELECT * FROM pet_description;
+
+SELECT * FROM volunteers;
+
+
+---- path="/pets" ---- getAllPets() ---- 
+SELECT * FROM pets;
+
+---- path="/pets/{id}" ---- Pet getPet(int petId) ---- Pet IDs are from 1000-1999
+SELECT * FROM pets WHERE pet_id = 1001;
+SELECT * FROM pets WHERE pet_id = 1002;
+
+---- no endpoint ---- List<String> getPetDescriptions(int petId) ---- Pet IDs are from 1000-1999
+SELECT p.name, d.description FROM descriptions d
+JOIN pet_description pd ON pd.description_id = d.description_id
+JOIN pets p ON p.pet_id = pd.pet_id
+WHERE p.pet_id = 1002;
+
+
