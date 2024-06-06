2914b975955c94677423ad6dbcbd2f7199dbc219
Koi McCormick <koimcc002@gmail.com>
2024-06-06 14:11:55 -0700

    Home page style and button function updates

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="capstone-app">
     <div id="nav">
-      
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <table class="header">
+      <thead>
+        <tr>
+          <th><router-link id="header-title-name" v-bind:to="{ name: 'home' }"> Sheltered Furry Friends </router-link></th>
+          <th id="header-home-link">Home</th>
+          <th id="header-about-link">About</th>
+          <th id="header-contact-link">Contact Us</th>
+          <th id="header-sign-in">
+            <button id="header-sign-in-button">
+              <router-link id="login" v-bind:to="{ name: 'login' }"> Sign In </router-link>
+            </button>
+          </th>
+        </tr>
+      </thead>
+    </table>
+      <!-- <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp; -->
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
@@ -11,5 +25,29 @@
 
 <style>
   /* @import '~vuetify/src/stylus/main' */
+
+.header {
+  width: 100%;
+  font-size: x-large;
+
+}
+
+#header-sign-in-button {
+  border-radius: 25px;
+  padding: 10px;
+  background-color: black;
+  
+  
+}
+
+#header-title-name {
+  color: inherit; 
+  text-decoration: none;
+}
+
+#login {
+  color: white;
+  text-decoration: none;
+}
 
 </style>
