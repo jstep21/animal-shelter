9ea1220c5cd4a28c56eab940481f245147d2cba2
Maria Kofinas <maria.kofinas@gmail.com>
2024-06-06 17:04:25 -0700

    login page style

--- vue/src/views/LoginView.vue
+++ vue/src/views/LoginView.vue
@@ -1,13 +1,14 @@
 <template>
-  <div id="login">
+  <div id="login" class="login-container" :style="{ backgroundImage: `url(${backgroundImage})` }">
     <form v-on:submit.prevent="login">
-      <h1 >Please Sign In</h1>
+      <h1>Please Sign In</h1>
       <div role="alert" v-if="invalidCredentials">
         Invalid username and password!
       </div>
       <div role="alert" v-if="this.$route.query.registration">
         Thank you for registering, please sign in.
       </div>
+      <div class="input-box">
       <div class="form-input-group">
         <label for="username">Username</label>
         <input type="text" id="username" v-model="user.username" required autofocus />
@@ -16,15 +17,18 @@
         <label for="password">Password</label>
         <input type="password" id="password" v-model="user.password" required />
       </div>
+      </div>
       <button type="submit">Sign in</button>
-      <p>
-      <router-link v-bind:to="{ name: 'register' }">Need an account? Sign up.</router-link></p>
+      <p class="center-text">
+        <router-link class="custom-link" v-bind:to="{ name: 'register' }">Need an account? Sign up.</router-link>
+      </p>
     </form>
   </div>
 </template>
 
 <script>
 import authService from "../services/AuthService";
+import homePageDogPic from "@/assets/Shelter Dog 2.jpg";
 
 export default {
   components: {},
@@ -34,7 +38,8 @@ export default {
         username: "",
         password: ""
       },
-      invalidCredentials: false
+      invalidCredentials: false,
+      backgroundImage: homePageDogPic
     };
   },
   methods: {
@@ -62,9 +67,103 @@ export default {
 
 <style scoped>
 .form-input-group {
-  margin-bottom: 1rem;
+  margin-bottom: .25rem;
+}
+
+label {
+  margin-right: .25remrem;
+}
+
+#login {
+  color: rgb(43, 98, 134);
+}
+
+h1 {
+  font-size: 2rem;
+  text-align: center;
+  margin-bottom: .5remrem;
+}
+
+button {
+  background-color: rgb(43, 98, 134);
+  color: white;
+  padding: .25rem;
+  border-radius: 0.25rem;
+  border: none;
+  cursor: pointer;
+  margin-bottom: .5rem;
+  text-align: center;
+}
+
+button:hover {
+  background-color: rgb(41, 93, 128);
+}
+
+button:active {
+  background-color: rgb(41, 93, 128);
+}
+
+div {
+  display: flex;
+  padding: 10px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  max-width: 17rem;
+}
+
+input {
+  padding: 0.5rem;
+  margin: 0.25rem;
+  border: 1px solid #ccc;
+}
+
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+}
+
+.login-container::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: url('@/assets/Shelter Dog 2.jpg');
+  background-size: cover;
+  background-position: center;
+  opacity: 0.2;
+  z-index: -1;
+}
+
+.center-text {
+  text-align: center;
+}
+
+.custom-link {
+  color: rgb(7, 28, 41);
+}
+
+.custom-link:hover {
+  text-decoration: underline;
+}
+.input-box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 label {
-  margin-right: 0.5rem;
+  font-size: 1.25rem;
 }
-</style>
+  .form-input-group {
+    margin-bottom: 1rem;
+  }
+  </style>
